f809933eee7af4c4061058462d9663ba6b8859e5
SICSS-AMULaw <sicssamulaw@gmail.com>
2024-08-13 21:29:33 +0200

    New webpage

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-// scripts.js
-
-// Example JavaScript code if needed
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Document loaded');
-    // Add any JavaScript functionality here
-});
